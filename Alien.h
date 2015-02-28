3370da90c01067ebc74ced9d6c8c056fef17f790
jkassman <jkassman@nd.edu>
2015-02-27 22:57:34 -0500

    Re-Updated Alien.h

--- Alien.h
+++ Alien.h
@@ -1,10 +1,17 @@
 #ifndef ALIEN_H
 #define ALIEN_H
 
+#include "Player.h"
+
 class Alien {
     public:
 	Alien();
 	int getInput();
+	bool challenge(int alienTrait, int playerTrait);
+	bool decideGood(int input, int encounter);
+	virtual void startEncounter(&Player) =0;
+	virtual void displayEncounterText(int encounter) =0;
+	void updateStats(int input, bool win);
 
     protected:
 	int trait[9];
