1cda2d1ef8e8d9357471acdcf7633f20a102868d
lkuta <lkuta@nd.edu>
2015-03-12 21:01:59 -0400

    Update to the Player.h and Player.cpp files

--- Player.cpp
+++ Player.cpp
@@ -1,27 +1,104 @@
+/* Lauren Kuta
+   CSE 20212
+   Player.cpp
+   Contains defintions of functions initialized in the Player.h file
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+#include <cstdlib>
 #include "Player.h"
 
+using namespace std;
+
 Player::Player() {
-  for (int i = 0; i < 8; i++) {
-    trait[i] = 1;
-    if (i < 4) {
-      attribute[i] = 1;
-    }
+  srand(time(NULL));
+  for(int i = 0; i <= 4; i++) {
+    Attributes.push_back(6);
+   }
+ // Attributes: 0 = Energy, 1 = Intelligence, 2 = Engines, 3 = Sanity
+  for(int j = 0; j < 9; j++) {
+    Traits.push_back(rand() % 3 + 1);
+  }
+
+}
+
+void Player::printAttributes() {
+  int m;
+  for (m = 0; m < Attributes.size()-1; m++) {
+    cout << Attributes[m] << " ";
   }
 }
 
-//Based on which input was used and whether you won, update traits/attributes.
-//If you win: +1 trait, -1 opposite trait
-//if you lose: -randomish number in that attribute
-void Player::updateStats(int input, bool win) {
-  //nothing yet
+void Player::printTraits() {
+  int m;
+  for (m = 0; m < Traits.size()-1; m++) {
+    cout << Traits[m] << " ";
+  }
 }
 
-//The waiting stage in between encounters.
-void Player::upgradeAtt() {
-  //nothing yet
+int Player::getAttribute(int n) {
+   return Attributes[n-1];
 }
 
-//Return the chosen trait
-int Player::getTrait(int select) {
-  return trait[select]; //add range protection! (trait[1000] never happens)
+int Player::getTrait(int n) {
+   return Traits[n-1];
 }
+
+void Player::setAttribute(int pos, int value) {
+   Attributes[pos-1] = value;
+}
+
+void Player::setTrait(int pos, int value) {
+   Traits[pos-1] = value;
+}
+
+void Player::upgradeSanity() {
+  srand(time(NULL));
+  int raise, NewSan;
+  cout << "You decide to take a nap on your ratty cot to calm your nerves and improve your concentration." << endl << endl;
+  raise = rand() % 4 + 1;
+  NewSan = Attributes[3] + raise;
+  cout << "Your sanity level has increased " << raise << " points!" << endl;
+  cout << "Your total sanity is " << NewSan << "." << endl;
+  Attributes[3] = NewSan;
+}
+
+void Player::upgradeEngines() {
+  srand(time(NULL));
+  int raise, NewEng;
+  cout << "You double check that your engines are in working condition.  Looks like they'll last a little while longer." << endl << endl;
+  raise = rand() % 4 + 1;
+  NewEng = Attributes[2] + raise;
+  cout << "Your engines' level has increased " << raise << " points!" << endl;
+  cout << "The total level of your engines is " << NewEng << "." << endl;
+  Attributes[2] = NewEng;
+}
+
+void Player::upgradeIntel() {
+  srand(time(NULL));
+  int raise, NewIntel;
+  cout << "You flip through the pages of some the old lore books stached in a nook of the ship." << endl;
+  cout << "(you're too old fashioned to use the ship's databases)." << endl; 
+  cout << "Some of this information could be helpful for the encounters ahead!" << endl << endl;
+  raise = rand() % 4 + 1;
+  NewIntel = Attributes[1] + raise;
+  cout << "Your intelligence level has increased " << raise << " points!" << endl;
+  cout << "Your total intellgence level is " << NewIntel << "." << endl;
+  Attributes[1] = NewIntel;
+}
+
+void Player::upgradeEnergy() {
+  srand(time(NULL));
+  int raise, NewEner;
+  cout << "You fiddle with the power and shield systems aboard the ship." << endl;
+  cout << "These adjustments will definitely help in the long run." << endl << endl;
+  rais = rand() % 4 + 1;
+  NewEner = Attributes[0] + raise;
+  cout << "Your energy level has increased " << raise << "points!" << endl;
+  cout << "You total energy level is" << NewEner << "." << endl;
+  Attributes[0] = NewEner;
+}
+   
--- Player.h
+++ Player.h
@@ -1,15 +1,33 @@
+/* Lauren Kuta
+   CSE 20212
+   Player.h
+   Initializes the Player class, which contains data relevant to the human player
+*/
+
+#include <iostream>
+#include <vector>
 #ifndef PLAYER_H
 #define PLAYER_H
 
+using namespace std;
+
 class Player {
- public:
-  Player();
-  void upgradeAtt();
-  void updateStats(int input, bool win);
-  int getTrait(int select);
- private:
-  int trait[8];
-  int attribute[4];
+  public:
+    Player();
+    void printAttributes();	//For testing purposes only
+    void printTraits(); 	//For testing purposes only--as of right now
+    int getTrait(int);
+    int getAttribute(int);
+    void setTrait(int, int);
+    void setAttribute(int, int);
+    void upgradeSanity();
+    void upgradeEngines();
+    void upgradeIntel();
+    void upgradeEnergy();
+
+  private:
+    vector <int> Traits;
+    vector <int> Attributes;
 };
 
 #endif
