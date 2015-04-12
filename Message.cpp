9e47706e02a1eab1f11196297ae71531a217aec9
lkuta <lkuta@nd.edu>
2015-04-11 21:01:29 -0400

    Lauren Kuta 4/11/2015 9:00 PM
    Defined some of the functions in the Message class.
    
    To do:
    Add quit option during encounter
    Graphics!
    Lots of playtesting to balance incrTrait, hostility growth, etc.

--- Message.cpp
+++ Message.cpp
@@ -2,36 +2,80 @@
 #include <sstream>
 #include <iostream>
 #include <iomanip>
+#include <ctype.h>
+#include <cstdlib>
 #include "Message.h"
 using namespace std;
 
 
+void Message::intro()
+{
 
-void Mesasage::intro()
+}
+
+void Message::resetscreen(int * captain)
+{
+  int width = 3; //width of the traits; how much space padding to add.
+                 //for example, 3 does: |  1|, | 42|, and |123| 
+  system("clear");
+  cout << "POWER            Lasers |"
+       << setw(width) << captain[1]+captain[0] << "|"
+       << setw(width) << captain[2]+captain[0]
+       << "| Shields"<< endl;
+  cout << "INTELLIGENCE   Trickery |"
+       << setw(width) << captain[3] + captain[1] <<"|"
+       << setw(width) << captain[4] + captain[1] 
+       <<"| Diplomacy"<<endl;
+  cout << "ENGINES           Speed |"
+       << setw(width) << captain[5] + captain[2] <<"|"
+       << setw(width) << captain[6] + captain[2] 
+       <<"| Navigation"<<endl;
+  cout << "SANITY          Bravery |"
+       << setw(width) << captain[7] + captain[3] <<"|"
+       << setw(width) << captain[8] + captain[3] 
+       <<"| Caution"<<endl;
+
+  cout << endl;
+}
+
+void Message::trainAttr(char attr)
+{
+  switch (toupper(attr)) {
+    case 'S':
+      cout << "You decide to take a nap on your ratty cot to calm your nerves and improve your concentration." << endl << endl;
+      break;
+    case 'E':
+      cout << "You double check that your engines are in working condition.  Looks like they'll last a little while longer." << endl << endl;
+      break;
+    case 'I':
+      cout << "You flip through the pages of some the old lore books stached in a nook of the ship." << endl;
+      cout << "(you're too old fashioned to use the ship's databases)." << endl; 
+      cout << "Some of this information could be helpful for the encounters ahead!" << endl << endl;
+      break;
+    case 'P':
+      cout << "You fiddle with the power and shield systems aboard the ship." << endl;
+      cout << "These adjustments will definitely help in the long run." << endl << endl;
+      break;
+    default:
+      cout << "This should never happen." << endl;
+      break;
+  } 
+}
+
+void Message::newEncounter(int encType, string alienName)
+{
+  
+}
+
+void Message::askTrait(int encType)
 {
 }
 
-void Mesasage::resetscreen(int * traits)
+void Message::encResults(int encType, int trait, bool win, string alienName)
 {
 }
 
-void Mesasage::trainAttr(int attr)
-{
-}
-
-void Mesasage::newEncounter(int encType, string alienName)
-{
-}
-
-void Mesasage::askTrait(int encType)
-{
-}
-
-void Mesasage::encResults(int encType, int trait, bool win, string alienName)
-{
-}
-
-void Mesasage::getMad(string alienName)
+void Message::getMad(string alienName)
 {
 }
 
--- Message.h
+++ Message.h
@@ -2,17 +2,19 @@
 #define MESSAGE_H
 
 #include <string>
+using namespace std;
+
 class Message
 {
 	public:
 	  void intro();
 	  void resetscreen(int *);
-	  void trainAttr(int);
+	  void trainAttr(char);
 	  void newEncounter(int, string);
 	  void askTrait(int);
 	  void encResults(int, int, bool, string);
 	  void getMad(string);
-}
+};
 
 
 
