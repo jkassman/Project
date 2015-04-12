88caf9d15fcd57e949bf0482cf3f0edc0f73736d
jkassman <jkassman@nd.edu>
2015-04-11 20:49:43 -0400

    Added static myZone to encounter
    -other small changes

--- Encounter.h
+++ Encounter.h
@@ -8,8 +8,8 @@
 class Encounter {
  public:
   int getTraitInput();
-  bool challenge(int alienTrait, int playerTrait); //not coded
-  bool decideGood(int input, int encounter); //not coded
+  bool challenge(int alienTrait, int playerTrait);
+  bool decideGood(int input, int encounter);
   bool start(Alien*, Player*);
   char firstCharEntered(char* second);
   void displayResult(bool win, int encounter, int trait);
@@ -17,7 +17,10 @@ class Encounter {
   string encounter2str(int);
   string trait2str(int);
   string attribute2str(int);
-  //void resetScreen(Player*, Alien*);
+  int getZone();
+  void changeZone(int select);
+ private:
+  static int myZone;
 };
 
 #endif
